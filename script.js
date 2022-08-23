9077b2b81349516958cc8db64f1af93797bf373b
maiyonnaise <dxuanmai43@gmail.com>
2022-08-23 14:21:13 +0700

    everything done except for img slide

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const closeBtn = document.querySelector('.nav-close-btn')
 const openBtn = document.querySelector('.nav-open-btn')
 const navbar = document.querySelector('.navbar')
+const header = document.querySelector('.header')
+const forwardBtns = document.getElementsByClassName('forward-btn')
 
+const backBtns = document.getElementsByClassName('back-btn')
 
 
 closeBtn.addEventListener('click', closeSidebar)
@@ -13,3 +16,18 @@ function closeSidebar() {
   navbar.classList.remove('active')
 }
 
+window.onscroll = () => {
+
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    header.style.backgroundColor = 'white'
+  } else {
+    header.style.backgroundColor = 'transparent'
+  }
+}
+
+window.addEventListener('resize', () => {
+  // if (window.innerWidth >= 1024) {
+  //   console.log(forwardBtns)
+  // }
+})
---
+++ style-guide.md
@@ -0,0 +1,18 @@
+hsla(174, 62%, 47%, 0.6)
+hsla(174, 62%, 47%, 0)
+
+hsla(353, 81%, 51%, 0.6)
+hsla(353, 81%, 51%, 0)
+
+#FFF3F5
+background: linear-gradient(90deg, rgba(255, 230, 230, 0.52) 0%, rgba(245, 255, 254, 0.52) 26.81%, rgba(252, 243, 255, 0.52) 54.58%, rgba(255, 244, 230, 0.52) 99.86%);
+
+
+change button size when viewport size change to an extent 
+
+
+background: linear-gradient(#ffffff, #ffffff) padding-box,
+        linear-gradient(180deg, rgba(231, 29, 54, 0.6) 0%, rgba(231, 29, 54, 0) 100%) border-box;
+
+background: linear-gradient(#f7fffe, #f7fffe) padding-box,
+        linear-gradient(180deg, rgba(46, 196, 182, 0.6) 0%, rgba(46, 196, 182, 0) 100%) border-box;
